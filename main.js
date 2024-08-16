50346975b2c23b0b743fca42a64d3da3e045a50b
tonju <tonju112420@gmail.com>
2024-08-16 14:12:25 +0800

    feat:: added gameover window

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import "./index.css";
 
 const hangmanImg = document.querySelector("#hangman-img");
+const incorrectGuesses = document.querySelector(".counter");
+
 
 hangmanImg.src = 'svg/hangman1.svg';
 
